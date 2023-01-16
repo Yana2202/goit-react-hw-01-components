750640ea3c3546f94db81dcee92853978ef7baea
Yana2202 <107585490+Yana2202@users.noreply.github.com>
2023-01-16 23:15:05 +0200

    hw-task-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
 import user from '../data/user.json';
-
-
+import data from '../data/data.json';
 export const App = () => {
     return (
       <>
@@ -12,6 +12,7 @@ export const App = () => {
           avatar={user.avatar}
           stats={user.stats}
         />
+   <Statistics title="Upload stats" stats={data} />
       </>
     );
   };
