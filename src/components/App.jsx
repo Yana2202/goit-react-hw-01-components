e6e3fc1060e6ba6166c0db22aa21d9ab83e5b35c
Yana2202 <107585490+Yana2202@users.noreply.github.com>
2023-01-17 14:51:57 +0200

    hw-task-4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
 import FriendsList from './FriendsList/FriendsList';
-// import FriendsList from './FriendsList/FriendsList'
+import Transactions from './Transactions/Transactions';
+
 import user from '../data/user.json';
 import data from '../data/data.json';
 import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 export const App = () => {
     return (
       <>
@@ -17,6 +19,7 @@ export const App = () => {
         />
    <Statistics title="Upload stats" stats={data} />
    <FriendsList friends={friends} />;
+   <Transactions items={transactions} />;
       </>
     );
   };
