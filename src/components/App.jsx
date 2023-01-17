ac04c8db89ace7070e19345392a126bc4cc8991c
Yana2202 <107585490+Yana2202@users.noreply.github.com>
2023-01-17 14:34:35 +0200

    hw-task-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,10 @@
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
+import FriendsList from './FriendsList/FriendsList';
+// import FriendsList from './FriendsList/FriendsList'
 import user from '../data/user.json';
 import data from '../data/data.json';
+import friends from '../data/friends.json';
 export const App = () => {
     return (
       <>
@@ -13,6 +16,7 @@ export const App = () => {
           stats={user.stats}
         />
    <Statistics title="Upload stats" stats={data} />
+   <FriendsList friends={friends} />;
       </>
     );
   };
--- src/components/FriendsList/FriendsList.jsx
+++ src/components/FriendsList/FriendsList.jsx
@@ -1,1 +1,31 @@
+import FriendsListItem from 'components/FriendsListItem/FriendsListItem';
 import PropTypes from 'prop-types';
+import style from './FriendsList.module.css';
+
+const FriendsList = ({ friends }) => {
+  return (
+    <>
+      <ul className={style.friendsList}>
+        {friends.map(({ id, avatar, name, isOnline }) => (
+          <FriendsListItem
+            key={id}
+            avatar={avatar}
+            name={name}
+            isOnline={isOnline}
+          />
+        ))}
+      </ul>
+    </>
+  );
+};
+FriendsList.defaultProps = {
+  friends: [],
+};
+FriendsList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+export default FriendsList;
